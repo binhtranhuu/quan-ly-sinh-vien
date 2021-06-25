572d4814a152a2404bc8766fef7b9c07dc8c7ab3
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-25 22:21:37 +0700

    2. Thiết kế giao diện trang chủ

--- app/src/main/java/com/example/qlsv/MainActivity.java
+++ app/src/main/java/com/example/qlsv/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.qlsv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTimKiem, btnSinhVien, btnLopHoc, btnDangKyHoc, btnThongKe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTimKiem = findViewById(R.id.btnTimKiem);
+        btnSinhVien = findViewById(R.id.btnSinhVien);
+        btnLopHoc = findViewById(R.id.btnLopHoc);
+        btnDangKyHoc = findViewById(R.id.btnDangKyHoc);
+        btnThongKe = findViewById(R.id.btnThongKe);
+
+        btnTimKiem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManTimKiem.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSinhVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManStudent.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLopHoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManClass.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDangKyHoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManStudentClass.class);
+                startActivity(intent);
+            }
+        });
+
+        btnThongKe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManThongKe.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/qlsv/ManStudentClass.java
@@ -0,0 +1,14 @@
+package com.example.qlsv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManStudentClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_student_class);
+    }
+}
