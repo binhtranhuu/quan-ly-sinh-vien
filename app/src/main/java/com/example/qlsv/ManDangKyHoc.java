d042521d07471bdb1904084d2ec4a3b0263c1dba
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-25 22:53:48 +0700

    8. Hiển thị danh sách lớp học trong ManStudentClass

---
+++ app/src/main/java/com/example/qlsv/ManDangKyHoc.java
@@ -0,0 +1,14 @@
+package com.example.qlsv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManDangKyHoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_dang_ky_hoc);
+    }
+}
--- app/src/main/java/com/example/qlsv/ManStudentClass.java
+++ app/src/main/java/com/example/qlsv/ManStudentClass.java
@@ -2,13 +2,50 @@ package com.example.qlsv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.qlsv.adapter.adapterClass;
+import com.example.qlsv.database.database;
+import com.example.qlsv.model.Class;
+
+import java.util.ArrayList;
 
 public class ManStudentClass extends AppCompatActivity {
+
+    ListView lvListClass;
+    Button btnAddStudentClass;
+
+    ArrayList<Class> classArrayList;
+
+    adapterClass adapterClass;
+
+    database databaseQLSV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_student_class);
+
+        databaseQLSV =  new database(this);
+
+        // Ánh xạ
+        lvListClass = findViewById(R.id.lvListClass);
+        btnAddStudentClass = findViewById(R.id.btnAddStudentClass);
+
+        btnAddStudentClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManStudentClass.this, ManDangKyHoc.class);
+                startActivity(intent);
+            }
+        });
+
+        classArrayList = databaseQLSV.getAllClass();
+        adapterClass = new adapterClass(getApplicationContext(), classArrayList);
+        lvListClass.setAdapter(adapterClass);
     }
 }
